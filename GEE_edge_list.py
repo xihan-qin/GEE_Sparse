8e4fcd6d3531e51b58cef5c481c5c78cfa264348
xihan-qin <nodyqxh@gmail.com>
2023-01-10 14:15:27 -0500

    add scripts

---
+++ GEE_edge_list.py
@@ -0,0 +1,146 @@
+# -*- coding: utf-8 -*-
+################################################################################
+import sys
+import numpy as np
+import copy
+from numpy import linalg as LA
+from tensorflow import keras
+from tensorflow.keras.utils import to_categorical
+from sklearn.metrics.cluster import adjusted_rand_score
+from sklearn.cluster import KMeans
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn import metrics
+import time
+# for sparse matrix
+from scipy import sparse
+#early stop
+from keras.callbacks import EarlyStopping
+from tensorflow.keras.callbacks import ModelCheckpoint
+################################################################################
+
+# invalide devide resutls will be nan
+np.seterr(divide='ignore', invalid='ignore')
+
+############------------graph_encoder_embed_start----------------###############
+class GraphEncoderEmbed_Edge:
+  def run(self, X, Y, n, **kwargs):
+    defaultKwargs = {'DiagA': True, 'Laplacian': False, 'Correlation': True}
+    kwargs = { **defaultKwargs, **kwargs}
+
+    X = X.copy()
+    Y = Y.copy()
+    
+    X = self.to_s3_list(X)
+    
+    emb_strat = time.time()
+
+    if kwargs['DiagA']:
+      X = self.Diagonal(X, n)
+
+    if kwargs['Laplacian']:
+      X = self.Laplacian(X, n)
+    
+    Z, W = self.Basic(X, Y, n)
+
+    if kwargs['Correlation']:
+      Z = self.Correlation(Z, n)
+    
+    emb_end = time.time()
+    emb_time = emb_end - emb_strat
+    
+    return Z, W, emb_time
+
+  def Basic(self, X, Y, n):
+    """
+      graph embedding basic function
+      input X is S3 edge list
+      input Y is numpy array with size (n,1):
+      -- value -1 indicate no lable
+      -- value >=0 indicate real label
+      input n: number of vertices
+    """
+    k = Y[:,0].max() + 1
+
+    Z = np.zeros((n,k))   
+    #nk: 1*n array, contains the number of observations in each class
+    #W: encoder marix. W[i,k] = {1/nk if Yi==k, otherwise 0}
+    nk = np.zeros((1,k))
+    W = np.zeros((n,k))
+
+    for i in range(k):
+      nk[0,i] = np.count_nonzero(Y[:,0]==i)
+
+    for i in range(Y.shape[0]):
+      k_i = Y[i,0]
+      if k_i >=0:
+        W[i,k_i] = 1/nk[0,k_i] ## GEE paper
+        # W[i,k_i] = nk[0,k_i]/n ## use as an example to show if people want to use nk/n instead of 1/nk
+ 
+    for row in X:
+      [v_i, v_j, edg_i_j] = row
+      v_i = int(v_i)
+      v_j = int(v_j)
+
+      label_i = Y[v_i][0] 
+      label_j = Y[v_j][0]
+
+      if label_j >= 0:
+        Z[v_i, label_j] = Z[v_i, label_j] + W[v_j, label_j]*edg_i_j
+      if (label_i >= 0) and (v_i != v_j):
+        Z[v_j, label_i] = Z[v_j, label_i] + W[v_i, label_i]*edg_i_j
+
+    return Z, W
+
+  def Diagonal(self, X, n):
+    # add self-loop to edg list -- add 1 connection for each (i,i)
+    self_loops = np.column_stack((np.arange(n), np.arange(n), np.ones(n)))
+    # faster than vstack --  adding the second to the bottom
+    X = np.concatenate((X,self_loops), axis = 0)
+    return X
+
+  def Laplacian(self, X, n):
+    s = X.shape[0] # get the row number of the edg list
+
+    D = np.zeros((n,1))
+    for row in X:
+      [v_i, v_j, edg_i_j] = row
+      v_i = int(v_i)
+      v_j = int(v_j)
+      D[v_i] = D[v_i] + edg_i_j
+      if v_i != v_j:
+        D[v_j] = D[v_j] + edg_i_j
+
+    D = np.power(D, -0.5)
+    
+    for i in range(s):
+      X[i,2] = X[i,2] * D[int(X[i,0])] * D[int(X[i,1])]
+
+    return X
+  
+  def Correlation(self, Z, n):
+    """
+      Calculate each row's 2-norm (Euclidean distance). 
+      e.g.row_x: [ele_i,ele_j,ele_k]. norm2 = sqr(sum(ele_i^2+ele_i^2+ele_i^2))
+      then divide each element by their row norm
+      e.g. [ele_i/norm2,ele_j/norm2,ele_k/norm2]
+    """
+    row_norm = LA.norm(Z, axis = 1)
+    reshape_row_norm = np.reshape(row_norm, (n,1))
+    Z = np.nan_to_num(Z/reshape_row_norm)
+
+    return Z
+    
+  def to_s3_list(self,X):
+    """
+      if input X only has 2 columns, make it into s3 edge list.
+      this function will return a s3 edge list
+      [node_i, node_j, weight]...
+    """
+    s = X.shape[0] # get the row number of the edg list
+    if X.shape[1] == 2:
+      # enlarge the edgelist to s*3 by adding 1 to the thrid position as adj(i,j)
+      X = np.insert(X, 1, np.ones(s,1))
+
+    return X
+
+############------------graph_encoder_embed_end------------------###############
---
+++ utils.py
@@ -0,0 +1,148 @@
+# -*- coding: utf-8 -*-
+################################################################################
+import copy
+import numpy as np
+################################################################################
+#-----------------input_data_from_file_start-----------------------------------#
+class DataSet:
+  def __init__(self):
+    self.X = None  # edg_list
+    self.n = None  # number of nodes
+    self.Y_ori = None  # original labels contains all original labels
+    self.Y = None  # unknown labels marked as -1, shape(n,1)
+    self.Y_train = None # labels for train set
+    self.train_idx = None # index for train set
+    self.Y_test = None # labels for test set
+    self.test_idx = None # index for test set
+  
+  def get_initial_values(self, edg_file, label_file_ori, label_file_train, label_file_test, stat_file):
+    resultSet = copy.deepcopy(self)
+
+    Y_ori, map_new_old_keys = self.read_node_ori_file(label_file_ori)
+    n = len(Y_ori)
+    Y_train, train_idx = self.read_node_other_file(label_file_train, map_new_old_keys)
+    Y_test, test_idx = self.read_node_other_file(label_file_test, map_new_old_keys)
+
+    X, edg_num = self.read_edge_file_with_remap(edg_file, map_new_old_keys)
+    k = self.get_k(Y_ori)
+
+    Y_ori = np.array(Y_ori).reshape(-1,1) # (n,1)
+    Y = self.mark_unknown_labels(Y_ori,test_idx)
+
+    test_ratio = self.read_stat_file(stat_file)
+
+    resultSet.X = X # edg_list
+    resultSet.edg_num = edg_num
+    resultSet.Y_ori = Y_ori
+    resultSet.Y = Y
+    resultSet.n = n
+    resultSet.Y_train = Y_train
+    resultSet.train_idx = train_idx 
+    resultSet.Y_test = Y_test 
+    resultSet.test_idx = test_idx     
+    resultSet.d = k
+    resultSet.A = self.edge_list_to_adjacency_matrix(X, n)
+    resultSet.test_ratio = test_ratio
+
+    return resultSet
+
+
+  def read_node_ori_file(self, filename):
+    """
+      node file with any node names
+
+    """
+    label_dict = {}
+    labels = open(filename, "r") 
+    map_new_old_keys = {}
+    
+    for l in labels:
+      (node_i, label_i) = l.strip().split("\t")
+      label_dict[node_i] = int(label_i)
+    
+    keys = sorted(list(label_dict.keys()))
+    new_node_idx = [i for i in range(len(keys))] # map the idx that starts with 0 with original keys (node name)
+    Y_ori = [] # stores the labels only
+    for i in range(len(keys)):
+      map_new_old_keys[keys[i]] = new_node_idx[i]
+      Y_ori.append(label_dict[keys[i]])
+    
+    return Y_ori, map_new_old_keys
+
+  def read_node_other_file(self, filename, map_new_old_keys):
+    """
+      based on the input file 
+      if test file, returns test_idx and test_labels
+      if train file, returns train_idx and train_labels 
+    """
+    idx_part = []
+    Y_part = []
+    labels = open(filename, "r") 
+    
+    for l in labels:
+      (node_i, label_i) = l.strip().split("\t")
+      new_key = map_new_old_keys[node_i]
+      idx_part.append(new_key)
+      Y_part.append(int(label_i))
+
+    return Y_part, idx_part
+
+  def read_edge_file_with_remap(self, filename, map_new_old_keys):
+    """
+      for the ids that are remaped from the node file, 
+      need to remap id for edge list as well
+    """
+    edg_list = []
+    edges = open(filename, "r") 
+
+    edg_num = 0
+    for l in edges:
+      edg_num += 1
+      elements = l.strip().split("\t")
+      if len(elements) > 2:
+        (node_i, node_j, w) = elements
+        new_idx_i = map_new_old_keys[node_i]
+        new_idx_j = map_new_old_keys[node_j]
+        edg_list.append([new_idx_i, new_idx_j, float(w)])
+      else: 
+        (node_i, node_j) = elements
+        new_idx_i = map_new_old_keys[node_i]
+        new_idx_j = map_new_old_keys[node_j]        
+        edg_list.append([new_idx_i, new_idx_j, 1]) 
+    edg = np.array(edg_list)
+    return edg, edg_num      
+
+  def get_k(self, Y_ori):
+    """
+      get the number of classes: k
+    """
+    k = len(set(Y_ori))
+    return k
+
+  def read_stat_file(self, filename):
+    stats = open(filename, "r") 
+    test_ratio = None
+    for l in stats:
+      if "test_ratio" in l:
+        elements = l.strip().split(": ")
+        test_ratio = elements[1]
+    return test_ratio  
+
+  def edge_list_to_adjacency_matrix(self, edg_list, n):
+    A = np.zeros((n,n))
+    for [i, j, w] in edg_list:
+      i = int(i)
+      j = int(j)
+      if A[i,j] != w:
+        A[i,j] = w
+      if A[j,i] != w:
+        A[j,i] = w
+    return A
+
+  def mark_unknown_labels(self, Y_ori, test_idx):
+      Y = Y_ori.copy()
+      # mark the test position as unknown: -1
+      Y[test_idx, 0] = -1    
+      return Y
+
+#-----------------input_data_from_file_end-----------------------------------#
